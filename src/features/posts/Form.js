592b967c1c36f63abbb5bdd9d15c725c09437bc8
joycethh <joycethh@outlook.com>
2022-11-07 15:26:24 -0600

    create usersSlice, used in the store, change the post structure to receive the userID

--- src/features/posts/Form.js
+++ src/features/posts/Form.js
@@ -1,25 +1,35 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded } from "./postsSlice";
+import { allUsers } from "../users/usersSlice";
 
 const Form = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+  const users = useSelector(allUsers);
+  console.log("users in the form", users);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
 
   const handleSubmit = () => {
     if (title && content) {
       //   dispatch(postAdded({ id: nanoid(), title, content }));
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
     }
   };
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
   return (
     <section>
       <h2>Add a New Post</h2>
@@ -33,7 +43,10 @@ const Form = () => {
           onChange={onTitleChanged}
         />
         <label htmlFor="postAuthor">Author:</label>
-
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
@@ -41,7 +54,7 @@ const Form = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={handleSubmit}>
+        <button type="button" onClick={handleSubmit} disabled={!canSave}>
           Submit
         </button>
       </form>
