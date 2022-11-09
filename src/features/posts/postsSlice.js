586af915790366c03734d07dbe99741b241a9549
joycethh <joycethh@outlook.com>
2022-11-09 13:18:21 -0600

    since we have "addPost" to interact with restAPI, we can now delete the "newPostAdded" reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -33,29 +33,6 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded: {
-      reducer(state, action) {
-        state.posts.push(action.payload);
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            title,
-            content,
-            userId,
-            date: new Date().toISOString(),
-            reactions: {
-              thumbsUp: 0,
-              wow: 0,
-              heart: 0,
-              rocket: 0,
-              coffee: 0,
-            },
-          },
-        };
-      },
-    },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
       const seletedPost = state.posts.find((post) => post.id === postId);
